f105ec327431ce2ad2dc61b840f09a42b270f83a
Amelia Heaton <ameliaheaton@MacBook-Pro.local>
2021-07-02 19:00:45 -0400

    Added .env.developemnt.local file for API key

--- team-git/src/SatelliteImage.js
+++ team-git/src/SatelliteImage.js
@@ -2,11 +2,9 @@ import React from 'react';
 
 const SatelliteImage = () => {
     <div>
-
+        <h3>Fetch Local Satellite Image from NASA</h3>
     </div>
 
 }
 
 export default SatelliteImage;
-
-//H289RgpfJ9E7R6aCnb9duEr58ida91fWMV2icKNt API key
