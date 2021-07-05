68bb788160ffcaa13841a12e6a477e81e4ed4a54
Angie Todd <a.m.todd999@gmail.com>
2021-07-05 19:52:02 -0400

    trying to update develop branch

--- team-git/src/App.js
+++ team-git/src/App.js
@@ -1,33 +1,33 @@
 import './App.css';
 import Header from './site/Header'
 import SatelliteImage from './components/SatelliteImage';
-import React, {Component, useState, useEffect} from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import WeatherApp from './components/WeatherApp';
 
 const App = () => {
 
   const [lat, setLat] = useState('');
   const [lon, setLon] = useState('');
-  
+
   const componentDidMount = () => {
-    navigator.geolocation.getCurrentPosition(function(position) {
+    navigator.geolocation.getCurrentPosition(function (position) {
       setLat(position.coords.latitude)
       setLon(position.coords.longitude)
       console.log(position.coords.latitude)
-  });
-}
-componentDidMount();
+    });
+  }
+  componentDidMount();
 
   return (
-    <div className="App">    
+    <div className="App">
 
       <Header />
-      <SatelliteImage 
+      <SatelliteImage
         lat={lat}
         lon={lon}
       />
-       <WeatherApp />
-    </div>  
+      <WeatherApp />
+    </div>
   );
 }
 
