1dd51f832bd3482a056467c1187920b957f0a530
Amelia Heaton <ameliaheaton@MacBook-Pro.local>
2021-07-05 19:32:05 -0400

    Added styling and assets folder with logo plus header.js

--- team-git/src/site/Header.js
+++ team-git/src/site/Header.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import logo from './assets/defenders-brain-hurt-api-logo.png'
+// import { Route, Link, Switch } from 'react-router-dom';
+import { Row, Col, Container } from 'react-bootstrap';
+// import SatelliteImage from '../components/SatelliteImage';
 
 const Header = () => {
     return(
-        <div>
-            <h1>Defenders of Fun</h1>
-      <p>Based on your location get the latest weather & events to plan out your day!</p>
+        <div className="headerBg">
+            {/* <img src={logo} alt="Logo" className="headerLogo" style={{width: "15%", height: "auto"}}/>
+        <p>Based on your location get the latest image, weather & events to plan out your day!</p> */}
+         <Container>  
+        <Row>
+    <Col><img src={logo} alt="Logo" className="headerLogo" style={{width: "15%", height: "auto"}}/></Col>
+    <Col><p>Based on your location get the latest image, weather & events to plan out your day!</p></Col>
+    <Col>blah</Col>
+  </Row>
+  </Container> 
+
         </div>
     )
 }
