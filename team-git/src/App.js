a3f3408ef6d829318ec975a3d1406e7525c7687e
Amelia Heaton <ameliaheaton@MacBook-Pro.local>
2021-07-05 22:32:47 -0400

    Added to CSS

--- team-git/src/App.js
+++ team-git/src/App.js
@@ -4,6 +4,7 @@ import SatelliteImage from './components/SatelliteImage';
 import React, { Component, useState, useEffect } from 'react';
 import WeatherApp from './components/WeatherApp';
 import TicketMaster from './components/TicketMaster';
+import {Container, Row, Col,} from 'reactstrap';
 
 const App = () => {
 
@@ -20,12 +21,24 @@ const App = () => {
 
     return (
       <div className="App">
-      <Header />
-      <SatelliteImage lat={lat} lon={lon} />
-       <WeatherApp />
-       <TicketMaster lat={lat} lon={lon} />
-
-    </div>
+        
+            <Header />
+            <div className="mainDiv">
+              <div className="firstCol">
+                <h1 className="topTitles">Current Weather</h1>
+                <br />
+                <WeatherApp />
+                <br />
+               <SatelliteImage lat={lat} lon={lon} />
+              </div>
+              <div className="secondCol">
+              <h1 className="topTitles">Current Events</h1>
+              <br />
+                <TicketMaster lat={lat} lon={lon} />
+              </div>
+          </div>
+      </div>
+          
   );
 }
 
--- team-git/src/site/Header.js
+++ team-git/src/site/Header.js
@@ -1,22 +1,10 @@
 import React from 'react';
 import logo from './assets/defenders-brain-hurt-api-logo.png'
-// import { Route, Link, Switch } from 'react-router-dom';
-import { Row, Col, Container } from 'react-bootstrap';
-// import SatelliteImage from '../components/SatelliteImage';
 
 const Header = () => {
     return(
         <div className="headerBg">
-            {/* <img src={logo} alt="Logo" className="headerLogo" style={{width: "15%", height: "auto"}}/>
-        <p>Based on your location get the latest image, weather & events to plan out your day!</p> */}
-         <Container>  
-        <Row>
-    <Col><img src={logo} alt="Logo" className="headerLogo" style={{width: "15%", height: "auto"}}/></Col>
-    <Col><p>Based on your location get the latest image, weather & events to plan out your day!</p></Col>
-    <Col>blah</Col>
-  </Row>
-  </Container> 
-
+            <img src={logo} alt="Logo" className="headerLogo" style={{width: "15%", height: "auto"}}/>
         </div>
     )
 }
